0cb7fc151c7e6b74c71301f44626e9156b44f459
Magnus Homann <magnus@homann.se>
2017-02-26 19:23:45 +0100

    Added Swedish translation

---
+++ i18n/StandBrowser_sv.ts
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="sv_SE" sourcelanguage="">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="test_translations.py" line="48"/>
+        <source>Good morning</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>StandBrowser</name>
+    <message>
+        <location filename="stand_browser.py" line="226"/>
+        <source>&amp;Stand Browser</source>
+        <translation>&amp;Stand Browser</translation>
+    </message>
+    <message>
+        <location filename="stand_browser.py" line="188"/>
+        <source>Open browser...</source>
+        <translation>Öppna bläddrare...</translation>
+    </message>
+</context>
+<context>
+    <name>StandBrowserDockWidgetBase</name>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="26"/>
+        <source>Maturity Class</source>
+        <translation>Huggningsklass</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="40"/>
+        <source>Manage Class</source>
+        <translation>Målklass</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="50"/>
+        <source>Stand Index</source>
+        <translation>Ståndortsindex</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="60"/>
+        <source>Area</source>
+        <translation>Area</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="74"/>
+        <source>Age</source>
+        <translation>Ålder</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="84"/>
+        <source>Volume</source>
+        <translation>Volym</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="94"/>
+        <source>PSLBD</source>
+        <translation>TGLBÄ</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="104"/>
+        <source>Annual Increment</source>
+        <translation>Årlig tillväxt</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="117"/>
+        <source>Diameter</source>
+        <translation>Diameter</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="127"/>
+        <source>Stems</source>
+        <translation>Stamantal</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="137"/>
+        <source>Height</source>
+        <translation>Höjd</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="150"/>
+        <source>Updated</source>
+        <translation>Aktuell</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="163"/>
+        <source>Basal Area</source>
+        <translation>Grundyta</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="178"/>
+        <source>Layer</source>
+        <translation>Lager</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="190"/>
+        <source>Prev</source>
+        <translation>Förra</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="197"/>
+        <source>Next</source>
+        <translation>Nästa</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="220"/>
+        <source>Update</source>
+        <translation>Uppdatera</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="227"/>
+        <source>Prev Selected</source>
+        <translation>Förra valda</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="234"/>
+        <source>Next Selected</source>
+        <translation>Nästa valda</translation>
+    </message>
+</context>
+</TS>
