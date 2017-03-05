cb4256ba295734ddbd175f28057e257c98e6c6cb
Magnus Homann <magnus@homann.se>
2017-03-05 16:38:03 +0100

    Fix translation Makefile issues and update ts

--- i18n/StandBrowser_sv.ts
+++ i18n/StandBrowser_sv.ts
@@ -11,12 +11,12 @@
 <context>
     <name>StandBrowser</name>
     <message>
-        <location filename="stand_browser.py" line="226"/>
+        <location filename="stand_browser.py" line="231"/>
         <source>&amp;Stand Browser</source>
         <translation>&amp;Stand Browser</translation>
     </message>
     <message>
-        <location filename="stand_browser.py" line="188"/>
+        <location filename="stand_browser.py" line="190"/>
         <source>Open browser...</source>
         <translation>Öppna bläddrare...</translation>
     </message>
@@ -24,97 +24,92 @@
 <context>
     <name>StandBrowserDockWidgetBase</name>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="26"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="33"/>
         <source>Maturity Class</source>
         <translation>Huggningsklass</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="40"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="47"/>
         <source>Manage Class</source>
         <translation>Målklass</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="50"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="57"/>
         <source>Stand Index</source>
         <translation>Ståndortsindex</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="60"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="67"/>
         <source>Area</source>
         <translation>Area</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="74"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="81"/>
         <source>Age</source>
         <translation>Ålder</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="84"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="91"/>
         <source>Volume</source>
         <translation>Volym</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="94"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="101"/>
         <source>PSLBD</source>
         <translation>TGLBÄ</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="104"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="111"/>
         <source>Annual Increment</source>
         <translation>Årlig tillväxt</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="117"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="124"/>
         <source>Diameter</source>
         <translation>Diameter</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="127"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="134"/>
         <source>Stems</source>
         <translation>Stamantal</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="137"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="144"/>
         <source>Height</source>
         <translation>Höjd</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="150"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="157"/>
         <source>Updated</source>
         <translation>Aktuell</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="163"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="170"/>
         <source>Basal Area</source>
         <translation>Grundyta</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="178"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="177"/>
         <source>Layer</source>
         <translation>Lager</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="190"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="204"/>
         <source>Prev</source>
         <translation>Förra</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="197"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="211"/>
         <source>Next</source>
         <translation>Nästa</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="220"/>
-        <source>Update</source>
-        <translation>Uppdatera</translation>
-    </message>
-    <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="227"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="234"/>
         <source>Prev Selected</source>
         <translation>Förra valda</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="234"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="18"/>
         <source>Next Selected</source>
         <translation>Nästa valda</translation>
     </message>
