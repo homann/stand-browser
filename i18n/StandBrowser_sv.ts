6950ec68cf44ee1556eac29eb95d02c5ceb9c077
Magnus Homann <magnus@homann.se>
2017-03-13 21:32:51 +0100

    Added tooltip and translated

--- i18n/StandBrowser_sv.ts
+++ i18n/StandBrowser_sv.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="sv_SE" sourcelanguage="">
+<!DOCTYPE TS>
+<TS version="2.0" language="sv_SE">
 <context>
     <name>@default</name>
     <message>
@@ -19,109 +20,214 @@
 <context>
     <name>StandBrowserDockWidgetBase</name>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="36"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="39"/>
         <source>Maturity Class</source>
         <translation>Huggningsklass</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="50"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="56"/>
         <source>Manage Class</source>
         <translation>Målklass</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="64"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="73"/>
         <source>Stand Index</source>
         <translation>Ståndortsindex</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="78"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="90"/>
         <source>Area</source>
         <translation>Area</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="92"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="107"/>
         <source>Age</source>
         <translation>Ålder</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="106"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="124"/>
         <source>Volume</source>
         <translation>Volym</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="120"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="141"/>
         <source>PSLBD</source>
         <translation>TGLBÄ</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="134"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="158"/>
         <source>Annual Increment</source>
         <translation>Årlig tillväxt</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="154"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="181"/>
         <source>Diameter</source>
         <translation>Diameter</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="168"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="198"/>
         <source>Stems</source>
         <translation>Stamantal</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="182"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="215"/>
         <source>Height</source>
         <translation>Höjd</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="199"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="235"/>
         <source>Updated</source>
         <translation>Aktuell</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="216"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="259"/>
         <source>Basal Area</source>
         <translation>Grundyta</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="223"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="266"/>
         <source>Layer</source>
         <translation>Lager</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="275"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="331"/>
         <source>Prev</source>
         <translation>Förra</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="282"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="341"/>
         <source>Next</source>
         <translation>Nästa</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="298"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="363"/>
         <source>Prev Selected</source>
         <translation>Förra valda</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="21"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="24"/>
         <source>Next Selected</source>
         <translation>Nästa valda</translation>
     </message>
     <message>
         <location filename="stand_browser_dockwidget_base.ui" line="14"/>
         <source>Stand Browser</source>
-        <translation>&amp;Stand Browser</translation>
+        <translation>Stand Browser</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="289"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="351"/>
         <source>Help...</source>
         <translation>Hjälp...</translation>
     </message>
     <message>
-        <location filename="stand_browser_dockwidget_base.ui" line="254"/>
+        <location filename="stand_browser_dockwidget_base.ui" line="304"/>
         <source>Stand ID</source>
         <translation>Bestånds-ID</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="21"/>
+        <source>Go to next of the selected stands</source>
+        <translation>Gå till nästa valda bestånd</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="46"/>
+        <source>E.g. K2, R2</source>
+        <translation>T.ex. K2, R2</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="63"/>
+        <source>E.g. NO, NS, PG</source>
+        <translation>T.ex. NO, NS, PG</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="148"/>
+        <source>Tenths of each species, e.g. 08200 or X0000</source>
+        <translation>Tiondel av varje art, t.ex. 08200 eller X0000</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="273"/>
+        <source>Select active layer</source>
+        <translation>Välj aktivt lager</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="294"/>
+        <source>Stand identification</source>
+        <translation>Beståndsidentifikation</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="313"/>
+        <source>Optional comment</source>
+        <translation>Valfri kommentar</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="328"/>
+        <source>Go to previous stand</source>
+        <translation>Gå till föregående bestånd</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="338"/>
+        <source>Go to next stand</source>
+        <translation>Gå till nästa bestånd</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="348"/>
+        <source>Open help in browser</source>
+        <translation>Öppna hjälp i webläsare</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="360"/>
+        <source>Go to previous of the selected stands</source>
+        <translation>Gå till föregående valda bestånd</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="80"/>
+        <source>Specie and site index, e.g G28</source>
+        <translation>Art och ståndorstindex, t.ex. G28</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="97"/>
+        <source>Productive area [ha]</source>
+        <translation>Produktiv area [ha]</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="114"/>
+        <source>Mean tree total age (basal area weighted) [year]</source>
+        <translation>Grundytevägd medelålder [år]</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="131"/>
+        <source>Standing volume [m3sk/ha]</source>
+        <translation>Stående volym [m3sk/ha]</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="168"/>
+        <source>Annual volume increment [m3sk/ha]</source>
+        <translation>Årlig volymstillväxt [m3sk/ha]</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="188"/>
+        <source>Mean diameter (basal area weighted)[cm]</source>
+        <translation>Grundytevägd medeldiameter [cm]</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="205"/>
+        <source>Stem density [1/ha]</source>
+        <translation>Stamtäthet [1/ha]</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="222"/>
+        <source>Mean height (basal area weighted) [m] </source>
+        <translation>Grundytevägd medelhöjd [m]</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="242"/>
+        <source>Date last updated</source>
+        <translation>Datum för senaste uppdatering</translation>
+    </message>
+    <message>
+        <location filename="stand_browser_dockwidget_base.ui" line="249"/>
+        <source>Basal area [m2/ha]</source>
+        <translation>Grundyta [m2/ha]</translation>
     </message>
 </context>
 </TS>
